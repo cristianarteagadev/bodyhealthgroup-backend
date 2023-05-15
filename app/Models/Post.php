5eaa97841fd70d2b0adae6b1e89b6aef1571417a
Cristian Arteaga <cristian.arteaga.dev@gmail.com>
2023-05-14 22:25:51 -0400

    Modelos correspondientes al test configuradas apropiadamente

---
+++ app/Models/Post.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'tenant_id',
+        'property_id',
+        'rental_agreement',
+        'contract_period',
+        'currency',
+        'lease_price',
+        'lease_deposit_price',
+        'lease_deposit_installments',
+        'community_expenses_price',
+        'ipc_reset',
+        'payday',
+        'billing_notice',
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function region() {
+        return $this->belongsTo(Region::class);
+    }
+
+    public function links() {
+        return $this->hasMany(PostLinks::class);
+    }
+
+    public function tags() {
+        return $this->hasMany(PostTags::class);
+    }
+}

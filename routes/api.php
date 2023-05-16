6c47bbced4bcca8f1b08c9c7e13fd8076a495d38
Cristian Arteaga <cristian.arteaga.dev@gmail.com>
2023-05-16 15:00:43 -0400

    Configuracion de Rutas API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\RegionController;
 
-Route::apiResource('posts', 'PostController');
+//Categorias
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('category/{id}', [CategoryController::class, 'show']);
+Route::post('categories', [CategoryController::class, 'store']);
+Route::put('categories/{category}', [CategoryController::class, 'update']);
+Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+
+//Regiones
+Route::get('regions', [RegionController::class, 'index']);
+
+//Noticias
+Route::get('posts', [PostController::class, 'index']);
+Route::get('posts/{id}', [PostController::class, 'show']);
+Route::post('posts', [PostController::class, 'store']);
+Route::put('posts/{post}', [PostController::class, 'update']);
+Route::delete('posts/{post}', [PostController::class, 'destroy']);
